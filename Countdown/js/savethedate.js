b8f8b314a500076e04e178cc66544535bb8c4237
Maicon Giehl <maicongiehl@gmail.com>
2022-06-25 17:49:44 -0300

    dynamic-countdown

--- Countdown/js/savethedate.js
+++ Countdown/js/savethedate.js
@@ -1,41 +1,41 @@
 (function () {
-    const dateEventDOM = document.querySelector(".hero-content h1 span").innerText;
-    console.log(dateEventDOM);
+
+    setInterval(() => {
+        const dateEventDOM = document.querySelector(".hero-content h1 span").innerText;
+        const dateEvent = getDate(dateEventDOM);
+        
+        const today = new Date();
+        let left = dateEvent.getTime() - today.getTime();
+        
+        const oneMinute = 60 * 1000;
+        const oneHour = 60 * 60 * 1000;
+        const oneDay = 24 * oneHour;
+        
+        const daysLeft = parseInt(left / oneDay);
+        left = left - daysLeft * oneDay;
+        
+        const hoursLeft = parseInt(left / oneHour);
+        left = left - hoursLeft * oneHour;
+        
+        const minutesLeft = parseInt(left / oneMinute);
+        left = left - minutesLeft * oneMinute;
+        
+        const secondsLeft = parseInt(left / 1000);
+        
+        addLeftTime(daysLeft, hoursLeft, minutesLeft, secondsLeft);
+    }, 10);
     
-    const dateEvent = getDate(dateEventDOM);
-    console.log(dateEvent);
-    
-    const today = new Date ();
-    
-    let left = dateEvent.getTime() - today.getTime();
-
-    const oneMinute = 60 * 1000;
-    const oneHour = 60 * 60 * 1000; 
-    const oneDay = 24 * oneHour;
-
-    const daysLeft = parseInt(left / oneDay);
-    left = left - daysLeft * oneDay;
-
-    const hoursLeft = parseInt(left / oneHour);
-    left = left - hoursLeft * oneHour;
-
-    const minutesLeft = parseInt(left / oneMinute);
-    left = left - minutesLeft * oneMinute;
-
-    const secondsLeft = parseInt(left / 1000);
-    addLeftTime(daysLeft, hoursLeft, minutesLeft, secondsLeft)
-    
+    const p = document.createElement("p");
     function addLeftTime(d, h, m, s) {
-        const p = document.createElement("p");
         p.textContent = `Countdown: ${d} days, ${h} hours, ${m} minutes and ${s} seconds`;
         document.querySelector(".hero-content").appendChild(p);
     }
 
-    function getDate (str) {
+    function getDate(str) {
         const [date, hour] = str.split(" ");
         const [day, month, year] = date.split("/");
         const [h, m] = hour.split("H");
 
-        return new Date (year, month - 1, day, h, m);
+        return new Date(year, month - 1, day, h, m);
     }
-})()
+})();
